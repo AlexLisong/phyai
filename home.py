cebd2a8329a8c5c05e149c72df299a79dad77419
AlexLiSong <songhongli@gmail.com>
2019-07-29 23:30:05 -0700

    add upload base64

--- home.py
+++ home.py
@@ -5,6 +5,8 @@ from fastai.vision import *
 from fastai.metrics import error_rate
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import base64
+import json
 
 from werkzeug import secure_filename
 app = Flask(__name__)
@@ -13,38 +15,55 @@ CORS(app)
 def get_category(filename):
   defaults.device = torch.device('cpu')
   img = open_image('static/' + filename)
-# img
 
-  learn = load_learner('data')  
+  learn = load_learner('data')
 
   pred_class, pred_idx, outputs = learn.predict(img)
   return pred_class
 
 @app.route("/")
 def home():
-#   import json
-#   a = {'name':'Sarah', 'age': 24, 'isEmployed': True }
-#   return jsonify(a)
     return render_template('file.html')
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
-    #     response = flask.jsonify({'some': 'data'})
-    # response.headers.add('Access-Control-Allow-Origin', '*')
-    # return response
 
    if request.method == 'POST':
       f = request.files['file']
       name = secure_filename(f.filename)
       f.save(os.path.join('static', name))
-      import json
       category = get_category(name)
-      a = {'category': str(category)}
+      a = {'category': str(category), 'desc': '123'}
       print (a)
       data = jsonify(a)
       data.headers.add('Access-Control-Allow-Origin', '*')
       return data
 
+@app.route('/upload_b64', methods=['POST'])
+def upload_base64_file():
+
+    data = request.get_json()
+
+    if data is None:
+       print("No valid request body, json missing!")
+       return jsonify({'error': 'No valid request body, json missing!'})
+    else:
+
+       img_data = data['img']
+
+       convert_and_save(img_data)
+
+def convert_and_save(b64_string):
+    imgdata = base64.b64decode(b64_string)
+    name = secure_filename("imageToSave.jpg")
+    with open(name, 'wb') as f:
+        f.write(imgdata)
+    category = get_category(name)
+    a = {'category': str(category), 'desc': '123'}
+    print (a)
+    data = jsonify(a)
+    data.headers.add('Access-Control-Allow-Origin', '*')
+    return data
 
 
 if __name__ == '__main__':
